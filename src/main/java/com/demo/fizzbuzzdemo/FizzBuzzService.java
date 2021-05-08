4478e9f7f9bad0896f24cda91b59e2766f154e5a
Vishal <itsdesai42@gmail.com>
2021-05-08 17:30:49 +0530

    Print Numbers from 1 to 100 on Application start Numbers divisible by 3 should print 'Fizz' Numbers divisible by 5 should print 'Buzz' Numbers divisible by 3 & 5 should print 'FizzBuzz' Numbers not satisfying above condition should print same number

--- src/main/java/com/demo/fizzbuzzdemo/FizzBuzzService.java
+++ src/main/java/com/demo/fizzbuzzdemo/FizzBuzzService.java
@@ -5,14 +5,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class FizzBuzzService {
 
+    /*
+        Print FizzBuzz if number divisible by 3 & 5
+        Print Fizz if number divisible by 3
+        Print Buzz if number divisible by 5
+        Print number if none of the condition satisfied
+     */
+    public void printFizzBuzz() {
+        for (int num = 1; num <= 100; num++) {
+            System.out.println(evaluateNumber(num));
+        }
+    }
+
     public String evaluateNumber(int num) {
-        if(isNumberDivisibleBy3And5(num)){
+        if (isNumberDivisibleBy3And5(num)) {
             return "FizzBuzz";
         }
-        if(isNumberDivisibleBy3(num)){
+        if (isNumberDivisibleBy3(num)) {
             return "Fizz";
         }
-        if(isNumberDivisibleBy5(num)){
+        if (isNumberDivisibleBy5(num)) {
             return "Buzz";
         }
         return String.valueOf(num);
--- src/main/java/com/demo/fizzbuzzdemo/FizzbuzzdemoApplication.java
+++ src/main/java/com/demo/fizzbuzzdemo/FizzbuzzdemoApplication.java
@@ -1,13 +1,23 @@
 package com.demo.fizzbuzzdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class FizzbuzzdemoApplication {
+
+    @Autowired
+    FizzBuzzService fizzBuzzService;
 
     public static void main(String[] args) {
         SpringApplication.run(FizzbuzzdemoApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        fizzBuzzService.printFizzBuzz();
+    }
 }
