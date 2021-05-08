20819333aee3cf6cccc2ad1ce9ef221b04e63ff6
Vishal <itsdesai42@gmail.com>
2021-05-08 16:51:37 +0530

    Set up FizzBuzz Demo project

---
+++ src/main/java/com/demo/fizzbuzzdemo/FizzbuzzdemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.fizzbuzzdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FizzbuzzdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/fizzbuzzdemo/FizzbuzzdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.fizzbuzzdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
